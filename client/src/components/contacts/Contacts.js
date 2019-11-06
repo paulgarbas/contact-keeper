8cf7b36e1ec94ced937558b627d57f0e94001921
paulius.garbasauskas <paulius.garbasauskas@teltonika.lt>
2019-11-06 15:39:54 +0200

    CRUD without API

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,28 @@
+import React, { Fragment, useContext } from 'react';
+import ContactItem from './ContactItem';
+import Contactcontext from '../../context/contact/contactContext';
+
+const Contacts = () => {
+    const contactContext = useContext(Contactcontext);
+
+    const { contacts, filtered } = contactContext;
+
+    if (contacts.length === 0) {
+        return <h4>Please add a contact</h4>;
+    }
+    
+    return (
+        <Fragment>
+            {filtered !== null ? 
+                filtered.map(contact => (
+                    <ContactItem key={contact.id} contact={contact} />
+                )) : 
+                contacts.map(contact => (
+                    <ContactItem key={contact.id} contact={contact} />
+                ))
+            }
+        </Fragment>
+    )
+}
+
+export default Contacts;
