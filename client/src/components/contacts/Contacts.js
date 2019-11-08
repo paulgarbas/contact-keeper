4a2c3f7bf563c3355a84935cd49725841f58e4a1
paulius.garbasauskas <paulius.garbasauskas@teltonika.lt>
2019-11-08 14:46:05 +0200

    register user, load user

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactItem from './ContactItem';
 import Contactcontext from '../../context/contact/contactContext';
 
@@ -13,14 +14,20 @@ const Contacts = () => {
     
     return (
         <Fragment>
-            {filtered !== null ? 
-                filtered.map(contact => (
-                    <ContactItem key={contact.id} contact={contact} />
-                )) : 
-                contacts.map(contact => (
-                    <ContactItem key={contact.id} contact={contact} />
-                ))
-            }
+            <TransitionGroup>
+                {filtered !== null ? 
+                    filtered.map(contact => (
+                        <CSSTransition key={contact.id} timeout={1000} classNames="item">
+                            <ContactItem contact={contact} />
+                        </CSSTransition>
+                    )) : 
+                    contacts.map(contact => (
+                        <CSSTransition key={contact.id} timeout={1000} classNames="item">
+                            <ContactItem contact={contact} />
+                        </CSSTransition>
+                    ))
+                }
+            </TransitionGroup>
         </Fragment>
     )
 }
